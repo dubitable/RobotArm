06522ccf7052a77915ab1be1807f9260b75ba051
dubitable <pierrequereuil@gmail.com>
2021-06-26 16:17:30 -0700

    Port setup completed

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,6 +1,16 @@
+#include <Servo.h>
+
+int ports[6] = {3,5,6,9,10,11};
+
+const int NUM_SERVOS = 5;
+Servo servos[NUM_SERVOS];
+
 void setup() {
   // put your setup code here, to run once:
-
+  for (int i = 0; i < NUM_SERVOS; ++i){
+    servos[i].attach(ports[i]);
+  }
+  servos[0].write(90);
 }
 
 void loop() {
