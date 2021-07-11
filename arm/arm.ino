6eec758d22545b2a6ace6dd31aae5784baf0ba2c
dubitable <pierrequereuil@gmail.com>
2021-07-11 14:35:14 -0700

    Finally figured out how to write to servos.

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,16 +1,24 @@
 #include <Servo.h>
 
 int pins[6] = {3,5,6,9,10,11};
+Servo servo1;
 //
 
+void full_rotation() {
+  //do a full counter-clockwise rotation
+  servo1.write(0);
+  delay(1000);
+  servo1.write(90);
+  delay(1000);
+  servo1.write(180);
+  delay(1000);
+}
 void setup() {
   // put your setup code here, to run once:
-  Servo servo;
-  servo.attach(3);
-  servo.write(0);
+  servo1.attach(3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  full_rotation();
 }
