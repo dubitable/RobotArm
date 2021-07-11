d37279109d2342206b0bd123ec9035360621d720
dubitable <pierrequereuil@gmail.com>
2021-07-11 15:24:11 -0700

    Left and right functions added

--- arm/arm.ino
+++ arm/arm.ino
@@ -8,17 +8,27 @@ void full_rotation() {
   //do a full counter-clockwise rotation
   servo1.write(0);
   delay(1000);
-  servo1.write(90);
+  servo1.write(left(&servo1, 90));
   delay(1000);
-  servo1.write(180);
+  servo1.write(left(&servo1, 90));
   delay(1000);
 }
+
+int left(Servo *servo, int degrees){
+  return servo->read() + degrees;
+}
+
+int right(Servo *servo, int degrees){
+  return servo->read() - degrees;
+}
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   servo1.attach(3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  full_rotation();
+  //full_rotation();
 }
