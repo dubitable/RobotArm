8fd019a56f1df9d93c7a349d300dda437ad43644
dubitable <pierrequereuil@gmail.com>
2021-07-11 15:40:14 -0700

    Removed files from git

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,18 +1,25 @@
 #include <Servo.h>
 
+int SERVOS = 2;
 int pins[6] = {3,5,6,9,10,11};
-Servo servo1;
-//
+Servo servos[5];
 
-void full_rotation() {
-  //do a full counter-clockwise rotation
-  servo1.write(0);
+void rotation(int index, int amount){
+  amount = index + amount;
+  for (int i = index; i < amount; i++){
+    servos[i].write(0);
+  }
   delay(1000);
-  servo1.write(left(&servo1, 90));
+  for (int i = index; i < amount; i++){
+    servos[i].write(left(&servos[i], 90));
+  }
   delay(1000);
-  servo1.write(left(&servo1, 90));
+  for (int i = index; i < amount; i++){
+    servos[i].write(left(&servos[i], 90));
+  }
   delay(1000);
 }
+
 
 int left(Servo *servo, int degrees){
   return servo->read() + degrees;
@@ -25,10 +32,12 @@ int right(Servo *servo, int degrees){
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  servo1.attach(3);
+  for (int i = 0; i < SERVOS; i++){
+    servos[i].attach(pins[i]);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //full_rotation();
+  //rotation(0, 2);
 }
